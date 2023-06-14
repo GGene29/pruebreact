ca3d39b2d5cef3eb88e44405062a2a041ab978b9
GGene29 <sumozacalcurian@gmail.com>
2023-06-14 16:25:22 -0400

    header}

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,10 +2,12 @@
 
 function Content() {
   return (
-    <section className="container p-10 m-5 w-full h-1/2 flex justify-between">
-      <img src="./src/assets/img/descarga.jpeg" alt="misión" className="w-1/2 m-5 shadow-stone-600 rounded-md"/>
-      <p className="font-bold w-10/12 m-5">  <h2 className="text-xl">Misión</h2> <br/>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem unde necessitatibus ipsam? Reprehenderit quaerat voluptatibus inventore ipsa, explicabo asperiores magnam iure, possimus odit, similique vel? Aut dignissimos tempore earum!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iure accusamus quaerat repellendus eos natus praesentium reprehenderit, molestiae dolorem voluptas temporibus ipsa ab, libero modi ducimus aspernatur similique a odit?</p>
-    </section>
+    <>
+      <section className="container p-10 m-5 w-full h-1/2 flex justify-between">
+        <img src="./src/assets/img/descarga.jpeg" alt="misión" className="w-1/2 m-5 shadow-stone-600 rounded-md"/>
+        <p className="font-bold w-10/12 m-5">  <h2 className="text-xl">Misión</h2> <br/>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem unde necessitatibus ipsam? Reprehenderit quaerat voluptatibus inventore ipsa, explicabo asperiores magnam iure, possimus odit, similique vel? Aut dignissimos tempore earum!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iure accusamus quaerat repellendus eos natus praesentium reprehenderit, molestiae dolorem voluptas temporibus ipsa ab, libero modi ducimus aspernatur similique a odit?</p>
+      </section>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,12 @@ function Header() {
     
     return (
         <>
-            <img src="#" alt="fondo" className="w-auto"/>
             <h1 className="text-6xl font-extrabold text-gray-100">TURISMO<span className="text-green-800"> ECOLOGICO</span></h1>
+            <img src="./src/assets/img/descarga.jpeg" alt="fondo" className="w-full"/>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#000000" fill-opacity="1" d="M0,160L80,186.7C160,213,320,267,480,256C640,245,800,171,960,165.3C1120,160,1280,224,1360,256L1440,288L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+            </svg>
+            
         </>
     )
 
